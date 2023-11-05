308435112cc9f52ab17cee53be49c923abaf4b43
Gousesk198 <gousemastanvalishaik27@gmail.com>
2023-11-05 16:34:38 +0530

    setup new chainges

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+from typing import List
+
+setup(
+    name='YES-BANK-STOCK-CLOSING-PRICE-PREDECTION',
+    version='0.0.2',
+    author='Gouse Shaik',
+    author_email='gousemastanvalishaik27@gmail.com',
+    packages=find_packages()
+)
